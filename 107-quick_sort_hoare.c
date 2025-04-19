fc492a11537d6f831b8665ea82974ded8d0c9094
donnasaud <donnaalmadani@gmail.com>
2025-04-19 19:58:17 +0000

    task 11 hope it works

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -2,28 +2,31 @@
 #include <stdio.h>
 
 /**
- * swap_ints - Swaps two integers
- * @a: First int
- * @b: Second int
+ * swap - Swaps two integers in an array.
+ * @a: First integer
+ * @b: Second integer
  */
-void swap_ints(int *a, int *b)
+void swap(int *a, int *b)
 {
-	int tmp = *a;
-	*a = *b;
-	*b = tmp;
+	if (a != b)
+	{
+		int tmp = *a;
+		*a = *b;
+		*b = tmp;
+	}
 }
 
 /**
- * hoare_partition - Hoare partition scheme
+ * hoare_partition - Performs the Hoare partition scheme
  * @array: The array
- * @low: Start index
- * @high: End index
- * @size: Size of array
- * Return: New partition index
+ * @low: The low index
+ * @high: The high index
+ * @size: The size of the array
+ * Return: The partition index
  */
 int hoare_partition(int *array, int low, int high, size_t size)
 {
-	int pivot = array[low];
+	int pivot = array[high];
 	int i = low - 1, j = high + 1;
 
 	while (1)
@@ -39,34 +42,32 @@ int hoare_partition(int *array, int low, int high, size_t size)
 		if (i >= j)
 			return (j);
 
-		swap_ints(&array[i], &array[j]);
+		swap(&array[i], &array[j]);
 		print_array(array, size);
 	}
 }
 
 /**
- * quick_sort_hoare_recursive - Recursively sorts using Hoare scheme
+ * quick_sort_hoare_recursive - Recursive quicksort
  * @array: The array
- * @low: Lower index
- * @high: Upper index
- * @size: Size of array
+ * @low: The low index
+ * @high: The high index
+ * @size: The array size
  */
 void quick_sort_hoare_recursive(int *array, int low, int high, size_t size)
 {
-	int p;
-
 	if (low < high)
 	{
-		p = hoare_partition(array, low, high, size);
+		int p = hoare_partition(array, low, high, size);
 		quick_sort_hoare_recursive(array, low, p, size);
 		quick_sort_hoare_recursive(array, p + 1, high, size);
 	}
 }
 
 /**
- * quick_sort_hoare - Quick sort with Hoare partition scheme
+ * quick_sort_hoare - Sorts an array using Hoare partition scheme
  * @array: The array
- * @size: Size of the array
+ * @size: The size of the array
  */
 void quick_sort_hoare(int *array, size_t size)
 {
