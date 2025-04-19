cbe983475d6b561c1a6fdf88050749018573e172
donnasaud <donnaalmadani@gmail.com>
2025-04-19 20:09:35 +0000

    task 11 final fixes

--- 107-quick_sort_hoare.c
+++ 107-quick_sort_hoare.c
@@ -8,9 +8,9 @@
  */
 void swap_ints(int *a, int *b)
 {
-    int tmp = *a;
-    *a = *b;
-    *b = tmp;
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
 }
 
 /**
@@ -23,25 +23,25 @@ void swap_ints(int *a, int *b)
  */
 int hoare_partition(int *array, int low, int high, size_t size)
 {
-    int pivot = array[high];
-    int i = low - 1, j = high + 1;
+	int pivot = array[low];
+	int i = low - 1, j = high + 1;
 
-    while (1)
-    {
-        do {
-            i++;
-        } while (array[i] < pivot);
+	while (1)
+	{
+		do {
+			i++;
+		} while (array[i] < pivot);
 
-        do {
-            j--;
-        } while (array[j] > pivot);
+		do {
+			j--;
+		} while (array[j] > pivot);
 
-        if (i >= j)
-            return (j);
+		if (i >= j)
+			return (j);
 
-        swap_ints(&array[i], &array[j]);
-        print_array(array, size);
-    }
+		swap_ints(&array[i], &array[j]);
+		print_array(array, size);
+	}
 }
 
 /**
@@ -53,14 +53,14 @@ int hoare_partition(int *array, int low, int high, size_t size)
  */
 void quick_sort_hoare_recursive(int *array, int low, int high, size_t size)
 {
-    int p;
+	int p;
 
-    if (low < high)
-    {
-        p = hoare_partition(array, low, high, size);
-        quick_sort_hoare_recursive(array, low, p, size);
-        quick_sort_hoare_recursive(array, p + 1, high, size);
-    }
+	if (low < high)
+	{
+		p = hoare_partition(array, low, high, size);
+		quick_sort_hoare_recursive(array, low, p, size);
+		quick_sort_hoare_recursive(array, p + 1, high, size);
+	}
 }
 
 /**
@@ -70,9 +70,9 @@ void quick_sort_hoare_recursive(int *array, int low, int high, size_t size)
  */
 void quick_sort_hoare(int *array, size_t size)
 {
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    quick_sort_hoare_recursive(array, 0, size - 1, size);
+	quick_sort_hoare_recursive(array, 0, size - 1, size);
 }
 
